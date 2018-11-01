83e5b1b7826f0d5c1010bb46aa7683146ee866b2
ImZurix <36185818+ImZurix@users.noreply.github.com>
2018-11-01 14:39:42 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,12 +3,12 @@ const client = new Discord.Client();
 var prefix = "+";
 client.on('ready', () => {
    console.log(`----------------`);
-      console.log(`Cyhper Bot Script By : DREAM`);
+      console.log(`Idk`);
         console.log(`----------------`);
-      console.log(`ON ${client.guilds.size} Servers '     Script By : DREAM ' `);
+      console.log(`ON ${client.guilds.size} Servers ' Idk ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`TEXT`,"http://twitch.tv/Dream")
+client.user.setGame(``,"")
 client.user.setStatus("dnd")
 });
 
